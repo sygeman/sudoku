94b6ea376ceaf74fe207a658a97d82224adb9005
Anton Savitskiy <sygeman@gmail.com>
2022-07-28 15:17:19 +0200

    Updated nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  experimental: {
+    runtime: "experimental-edge",
+  },
 };
 
 module.exports = nextConfig;
