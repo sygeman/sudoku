1269bec5b729c378ee3bfa89f67bb55ed3a442ac
Anton Savitskiy <sygeman@gmail.com>
2022-07-31 22:46:03 +0200

    Added github link

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 export const Layout = ({ children }: { children?: React.ReactNode }) => (
-  <div className="flex items-center justify-center h-screen bg-slate-900 text-white">
+  <div className="flex relative items-center justify-center h-screen bg-slate-900 text-white">
+    <a
+      className="absolute right-4 top-2 text-slate-400 font-medium"
+      target="blank"
+      href="https://github.com/sygeman/sudoku"
+    >
+      Github
+    </a>
     {children}
   </div>
 );
