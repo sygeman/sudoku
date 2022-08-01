56fe6252e952f130ce7bd41fc034f59dd31fec2e
Tsvetkov Grigoriy <tsvetkov.g@jiffygrocery.co.uk>
2022-08-01 22:29:23 +0300

    refactoring cross.ts

--- src/utils/cross.ts
+++ src/utils/cross.ts
@@ -1,1 +1,2 @@
-export const cross = (a: string, b: string): string[] => a.split('').map((curr, index) => curr + b[index]);
+export const cross = (a: string, b: string): string[] =>
+  a.split('').map((curr, index) => curr + b[index]);
