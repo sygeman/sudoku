2235154358b77276812f5f41d0c343aa033114ec
Tsvetkov Grigoriy <tsvetkov.g@jiffygrocery.co.uk>
2022-08-01 22:41:44 +0300

    refactoring force-range.ts

--- src/utils/force-range.ts
+++ src/utils/force-range.ts
@@ -1,8 +1,4 @@
 /* Force `nr` to be within the range from `min` to, but not including, 
 `max`. `min` is optional, and will default to 0. If `nr` is undefined,
 treat it as zero. */
-export function forceRange(nr = 0, max: number, min = 0) {
-  if (nr < min) return min;
-  if (nr > max) return max;
-  return nr;
-}
+export const forceRange = (nr = 0, max: number, min = 0): number => Math.max(Math.min(nr, max), min);
