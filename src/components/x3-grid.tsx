c8d8ea49176895ddae8a503d44657de8095c8196
Anton Savitskiy <sygeman@gmail.com>
2022-08-16 18:33:06 +0200

    Updated X3Grid

--- src/components/x3-grid.tsx
+++ src/components/x3-grid.tsx
@@ -1,14 +1,17 @@
-import clsx from "clsx";
-import { JSX } from "solid-js";
+import { Component, JSX } from "solid-js";
 
 const x3Array = [...new Array(3)];
 
-export const X3Grid = (props: {
+export const X3Grid: Component<{
   renderCell: (rowIndex: number, cellIndex: number) => JSX.Element;
   gap?: 1 | 2;
-}) => (
+}> = (props) => (
   <div
-    class={clsx("grid grid-cols-3", props.gap === 1 ? "gap-0.5" : "gap-1.5")}
+    classList={{
+      "grid grid-cols-3": true,
+      "gap-0.5": props.gap === 1,
+      "gap-1.5": props.gap !== 1,
+    }}
   >
     {x3Array.map((_row, x3RowIndex) =>
       x3Array.map((_cell, x3CellIndex) => (
